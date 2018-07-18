8182737f520ac0cd2fcc02420e689f82cd494acc
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-18 05:16:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   #post-index_url.css(".student-card")
 
   def self.scrape_index_page(index_url)
-    doc= Nokogiri::HTML(open("index_url"))
+    doc= Nokogiri::HTML(open(index_url))
     doc.css(".student-card").map do |profile|
       {
         name: profile.css("h4.student-name").text,
